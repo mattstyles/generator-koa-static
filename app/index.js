8719e766d52f1c29f3d6fe698ff550a735f1d815
Matt Styles <matt@veryfizzyjelly.com>
2014-09-21 22:05:51 +0100

    #update - method/var names

--- app/index.js
+++ app/index.js
@@ -1,9 +1,8 @@
 'use strict';
-var util    = require('util');
-var path    = require('path');
-var fs      = require( 'fs' );
-var yeoman  = require('yeoman-generator');
-var yosay   = require('yosay');
+var util    = require( 'util' );
+var path    = require( 'path' );
+var yeoman  = require( 'yeoman-generator' );
+var yosay   = require( 'yosay' );
 
 module.exports  = yeoman.generators.Base.extend({
     init: function () {
@@ -16,7 +15,7 @@ module.exports  = yeoman.generators.Base.extend({
         });
     },
 
-    prompting: function () {
+    askFor: function () {
         var done = this.async();
 
         // Have Yeoman greet the user.
