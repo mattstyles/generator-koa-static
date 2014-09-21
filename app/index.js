18c66c871cc3843adf2b56cb3c4fdcfa1f82ce71
Matt Styles <matt@veryfizzyjelly.com>
2014-09-21 09:21:56 +0100

    #add - template files

--- app/index.js
+++ app/index.js
@@ -1,49 +1,52 @@
 'use strict';
 var util    = require('util');
 var path    = require('path');
+var fs      = require( 'fs' );
 var yeoman  = require('yeoman-generator');
 var yosay   = require('yosay');
 
 module.exports  = yeoman.generators.Base.extend({
-    initializing: function () {
-        this.pkg = require('../package.json');
+    init: function () {
+        this.pkg = require( '../package.json' );
     },
 
     prompting: function () {
         var done = this.async();
 
         // Have Yeoman greet the user.
-        this.log(yosay(
-            'Welcome to the unreal GeneratorKoaStatic generator!'
+        this.log( yosay(
+            'Welcome to the Koa-Static generator!'
         ));
 
-        var prompts = [{
-            type: 'confirm',
-            name: 'someOption',
-            message: 'Would you like to enable this option?',
-            default: true
-        }];
+        // var prompts = [{
+        //     type: 'confirm',
+        //     name: 'someOption',
+        //     message: 'Would you like to enable this option?',
+        //     default: true
+        // }];
+        //
+        // this.prompt(prompts, function (props) {
+        //     this.someOption = props.someOption;
+        //
+        //     done();
+        // }.bind(this));
 
-        this.prompt(prompts, function (props) {
-            this.someOption = props.someOption;
-
-            done();
-        }.bind(this));
+        done();
     },
 
     writing: {
         app: function () {
-            this.dest.mkdir('app');
-            this.dest.mkdir('app/templates');
+            this.src.copy( 'package.json', 'package.json' );
+            this.src.copy( 'bower.json', 'bower.json' );
+            this.src.copy( '.bowerrc', '.bowerrc' );
+            this.src.copy( 'README.md', 'README.md' );
 
-            this.src.copy('_package.json', 'package.json');
-            this.src.copy('_bower.json', 'bower.json');
+            // @TODO: be explicit
+            this.directory( 'bin', 'bin' );
+            this.directory( 'public', 'public' );
+            this.directory( 'lib', 'lib' );
         },
 
-        projectfiles: function () {
-            this.src.copy('editorconfig', '.editorconfig');
-            this.src.copy('jshintrc', '.jshintrc');
-        }
     },
 
     end: function () {
