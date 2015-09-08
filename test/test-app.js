c527c6ed53bca0b4127eaccfc81ac190871c4dc5
Matt Styles <matt@veryfizzyjelly.com>
2015-09-08 16:25:40 +0100

    #update test cases

--- test/test-app.js
+++ test/test-app.js
@@ -1,26 +1,31 @@
 /*global describe, beforeEach, it*/
-'use strict';
+'use strict'
 
-var path = require('path');
-var assert = require('yeoman-generator').assert;
-var helpers = require('yeoman-generator').test;
-var os = require('os');
+var path = require('path')
+var assert = require('yeoman-generator').assert
+var helpers = require('yeoman-generator').test
+var os = require('os')
 
 describe('generator-koa-static:app', function () {
     before(function (done) {
         helpers.run(path.join(__dirname, '../app'))
             .inDir(path.join(os.tmpdir(), './temp-test'))
             .withOptions({ 'skip-install': true })
-            .withPrompt({
-                someOption: true
+            .withPrompts({
+                projectName: 'test-project',
+                projectDescription: 'test-description',
+                authorName: 'test-author',
+                userName: 'test-user',
+                license: 'WTFPL'
             })
-            .on('end', done);
-    });
+            .on('end', done)
+    })
 
     it('creates files', function () {
         assert.file([
-            'bower.json',
-            'package.json'
-        ]);
-    });
-});
+            'package.json',
+            'README.md',
+            'index.js'
+        ])
+    })
+})
